714e7bc677fa02b1707bd6c42f9f5d0987fd3705
droll-framework-1384 <ebyan.yusuf@gmail.com>
2020-11-09 19:25:41 +0000

    Done.

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,5 +1,19 @@
 def find_min_in_nested_arrays(src)
-  # src will be an array of arrays of integers
-  # Produce a new Array that contains the smallest number of each of the nested arrays
 
-end
+outer_results = []
+   row_index = 0
+   while row_index < src.length do
+   element_index = 0
+   current_lowest = src[row_index][element_index]
+   while element_index < src[row_index].length do
+     if src[row_index][element_index] < current_lowest
+       current_lowest = src[row_index][element_index]
+   end
+     element_index += 1
+   end
+   outer_results << current_lowest
+   row_index += 1
+ end
+ outer_results
+ 
+ end
