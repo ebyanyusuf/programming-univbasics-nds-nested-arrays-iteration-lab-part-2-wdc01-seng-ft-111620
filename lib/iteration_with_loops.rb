92cfcd366a2d50a8b3c6179598029dc06af76063
droll-framework-1384 <ebyan.yusuf@gmail.com>
2020-11-09 19:20:25 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,4 +1,10 @@
 def find_min_in_nested_arrays(src)
+ new_src
  
+ row_index = 0
+while row_index < src.count do
+  element_index = 0
+  src.min = ""
+  while element_index < spice_rack[row_index].count do
  
 end
